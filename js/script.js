db5648eb4d56e3c0807b61f5f85b3c892f863261
Joshua Bolanos <josh.bolanos7@gmail.com>
2021-09-16 13:16:32 -0400

    added more power stats to webpage

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const $input = $('input[type=text]');
 const $mainContent = $('main');
 const $image = $('#image');
 const $intelligence = $('#intelligence');
+const $combat = $('#combat');
+const $speed = $('#speed');
 
 function handleGetData(event) {
     event.preventDefault();
@@ -33,13 +35,26 @@ function handleGetData(event) {
         for(let stat in data.results[0].powerstats) {
             console.log(stat)
             console.log(data.results[0].powerstats[stat])
+            $powerstats.text(data.results[0].powerstats[stat])
         }
+        // for(let ga in data.results[0].connections) { 
+        //     console.log(ga)
+        //     console.log(data.results[0].connections[ga])
+            
+        // }
+        // for ( let api in data.results[0].appearance) { 
+        //     console.log(api);
+        //     console.log(data.results[0].appearance[api]);
+        //     $appearance.text(data.results[0].appearance[api]);
+        // }
         $name.text(data.results[0].name);
         $appearance.text(data.results[0].appearance.race)
         $biography.text(data.results[0].biography["full-name"]);
         $powerstats.text(data.results[0].powerstats.power);
         $intelligence.text(data.results[0].powerstats.intelligence);
         $image.attr('src', data.results[0].image["url"]);
+        $combat.text(data.results[0].powerstats.combat);
+        $speed.text(data.results[0].powerstats.speed);
 
         // $mainContent.append($img); 
 
